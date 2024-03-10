788d1e1fc28c5d55aea57c188be6dd0d3353f5d0
The Codesmith <b-9k@outlook.com>
2024-03-10 17:47:22 -0400

    Added EXIT ability.

--- nav_ninja.py
+++ nav_ninja.py
@@ -40,37 +40,44 @@ stylish_items += [
     '',
     "***********************",
     '',
-    f"UPDATE THIS LIST: {computer}_config.ini"
+    f"UPDATE THIS LIST: {computer}_config.ini",
+    '',
+    "EXIT\033[0m" # Exit is always last
 ]
 raw_paths += [
-    None,
-    None,
-    None,
+    None, # i
+    None, # i + 1
+    None, # i + 2
     f"{getcwd()}/{computer}_config.ini",
+    None, # i + 4
+    None  # i + 5
     ]
 captions += [
     i,
     i+1,
-    i+2
+    i+2,
+    i+4
 ]
 
 position = 2 # set cursor at first non-section item
-while True:
+exit_flag = 0
+while exit_flag == 0:
     system('cls')
     print("Hello " + computer.upper())
     position = select(
             stylish_items,
             caption_indices=captions,
-            deselected_prefix="   ",
-            selected_prefix=" \033[92m>\033[0m ",
+            deselected_prefix="\033[0m   ",
+            selected_prefix=" \033[92m>\033[7m\033[0m \033[7m", # 92 = green, 7 = reverse
             selected_index=position
         )
     
-    file = raw_paths[position]
-    prefix = '' if is_valid_website(file) else 'file:///' # if it's not a website, add prefix
-    open_url(prefix + file)
-    # TODO Add exit and auto commit
-    
-    # git add .
-    # git commit -m "config"
-    # git push
+    if position == len(stylish_items) - 1: # in other words, if selected position is last item
+        exit_flag = 1
+        # TODO git add .
+        # TODO git commit -m "config"
+        # TODO git push
+    else:
+        file = raw_paths[position]
+        prefix = '' if is_valid_website(file) else 'file:///' # if it's not a website, add prefix
+        open_url(prefix + file)
